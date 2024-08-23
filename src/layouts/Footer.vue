fedc670a450cad81f837de01d23751a12dae98af
Gabriel Ribeiro <49738156+GabrielSSRibeiro@users.noreply.github.com>
2024-08-23 07:56:54 -0400

    fixed footer hover

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -5,11 +5,9 @@
   <footer id="footer" class="bg-theme-dark-blue flex justify-center items-center p-10">
     <img class="z-10 w-1/4" src="/images/logo.png" />
     <div class="flex justify-center flex-col items-start ml-10">
-      <p class="uppercase text-white hover:text-theme-secondary text-xl">Conte com nossa assessoria jurídica!</p>
-      <p class="text-white hover:text-theme-secondary mt-1 mb-5">Nossa equipe de advogados especialistas está à disposição para te auxiliar!</p>
-      <p class="uppercase text-white hover:text-theme-secondary text-xs">
-        Copyright © 2024 - Frone & Soares Advocacia Especializada | Todos os direitos reservados.
-      </p>
+      <p class="uppercase text-white text-xl">Conte com nossa assessoria jurídica!</p>
+      <p class="text-white mt-1 mb-5">Nossa equipe de advogados especialistas está à disposição para te auxiliar!</p>
+      <p class="uppercase text-white text-xs">Copyright © 2024 - Frone & Soares Advocacia Especializada | Todos os direitos reservados.</p>
     </div>
   </footer>
   <!-- Footer End -->
